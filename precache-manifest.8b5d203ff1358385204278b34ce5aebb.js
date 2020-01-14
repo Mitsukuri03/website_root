f9c9db0caf70cb241f2d1a748a86414e578ccdf0
JorgeVieira <1150854@isep.ipp.pt>
2020-01-14 23:28:12 +0000

    Updates

---
+++ precache-manifest.8b5d203ff1358385204278b34ce5aebb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f4834784dbd0c2d7feee25584a424d",
+    "url": "/Mitsukuri03/website_root/index.html"
+  },
+  {
+    "revision": "21dac5b1d449d7e7421d",
+    "url": "/Mitsukuri03/website_root/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9b8c9d3efec3d3aad20",
+    "url": "/Mitsukuri03/website_root/static/js/2.eb09c538.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Mitsukuri03/website_root/static/js/2.eb09c538.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21dac5b1d449d7e7421d",
+    "url": "/Mitsukuri03/website_root/static/js/main.8f099220.chunk.js"
+  },
+  {
+    "revision": "eaefd217a19091d29300",
+    "url": "/Mitsukuri03/website_root/static/js/runtime-main.1d79d336.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mitsukuri03/website_root/static/media/logo.5d5d9eef.svg"
+  }
+]);
