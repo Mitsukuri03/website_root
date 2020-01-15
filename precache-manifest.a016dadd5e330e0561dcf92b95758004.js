506ff58c6a1ddc9e3796c03702b20cb7a7357360
JorgeVieira <1150854@isep.ipp.pt>
2020-01-15 00:17:11 +0000

    Updates

---
+++ precache-manifest.a016dadd5e330e0561dcf92b95758004.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61d24e9f23c1b33a17c8ef90c3ee1b4",
+    "url": "/website_root/index.html"
+  },
+  {
+    "revision": "5f4bded5811f375da4b5",
+    "url": "/website_root/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9b8c9d3efec3d3aad20",
+    "url": "/website_root/static/js/2.eb09c538.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/website_root/static/js/2.eb09c538.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f4bded5811f375da4b5",
+    "url": "/website_root/static/js/main.227ab642.chunk.js"
+  },
+  {
+    "revision": "bff8ef6312c99f636336",
+    "url": "/website_root/static/js/runtime-main.1a47884a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website_root/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Mitsukuri03/website_root/precache-manifest.7b5457f594288d85092f5e7e04894cc8.js"
+  "/website_root/precache-manifest.a016dadd5e330e0561dcf92b95758004.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mitsukuri03/website_root/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website_root/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
